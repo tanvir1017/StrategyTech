016e6bfcdf895b17a0cb7e384c6d7d69af3d80c0
Sif Fahim <siffahim25@gmail.com>
2022-01-03 16:47:52 +0600

    footer add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Signup from "./Components/Authentication/SignUp";
 import Blogs from "./Components/Blogs/Blogs";
 import ContactUs from "./Components/ContactUs/ContactUs";
 import Courses from "./Components/Courses/Courses";
+import Footer from './Components/Footer/Footer';
 import HomePage from "./Components/HomePage/Header";
 import Emergancy from "./Components/HomePage/OurITServices/NestedRoute/Emergancy";
 import RemoteOfficeSupport from "./Components/HomePage/OurITServices/NestedRoute/RemoteOfficeSupport";
@@ -18,29 +19,28 @@ import NotFound from "./Components/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route exact path="/" element={<HomePage />}></Route>
-          <Route path="/home" element={<HomePage />}>
-            <Route path="/home/e_support" element={<Emergancy />}></Route>
-            <Route
-              path="/home/r_o_support"
-              element={<RemoteOfficeSupport />}
-            ></Route>
-          </Route>
-          <Route path="/courses" element={<Courses />}></Route>
-          <Route path="/contact_us" element={<ContactUs />}></Route>
-          <Route path="/blogs" element={<Blogs />}></Route>
-          <Route path="/about_us" element={<AboutUs />}></Route>
-          <Route path="/ourItServices" element={<OurITServices />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route exact path="*" element={<NotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route exact path="/" element={<HomePage />}></Route>
+        <Route path="/home" element={<HomePage />}>
+          <Route path="/home/e_support" element={<Emergancy />}></Route>
+          <Route
+            path="/home/r_o_support"
+            element={<RemoteOfficeSupport />}
+          ></Route>
+        </Route>
+        <Route path="/courses" element={<Courses />}></Route>
+        <Route path="/contact_us" element={<ContactUs />}></Route>
+        <Route path="/blogs" element={<Blogs />}></Route>
+        <Route path="/about_us" element={<AboutUs />}></Route>
+        <Route path="/ourItServices" element={<OurITServices />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route exact path="*" element={<NotFound />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
