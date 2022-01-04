5a52708408d6d045aeec8fa21b23483aa39c8d61
tanvir1017 <tanvir.hossaindev1@gmail.com>
2022-01-04 12:27:59 +0600

    navbar responsive

--- src/Components/MyOrder/MyOrder.js
+++ src/Components/MyOrder/MyOrder.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import img from '../../images/order.gif';
 import useAuth from "../hooks/useAuth";
 import "./MyOrder.css";
 import MyOrderCard from "./MyOrderCard";
@@ -23,21 +22,32 @@ const MyOrder = () => {
 
   return (
     <div className="container">
-      {
-        orders.length > 0 ? <div className="row">
+      {orders.length > 0 ? (
+        <div className="row">
           {orders.map((order) => (
-            <MyOrderCard
-              key={order._id}
-              order={order}
-              setNum={setNum}
-            />
+            <MyOrderCard key={order._id} order={order} setNum={setNum} />
           ))}
-        </div> : <div>
-          <img src={img} alt="" />
-          <h2>Sorry, You have no order yet!</h2>
-          <Link to='/courses'>Please Go To some order</Link>
         </div>
-      }
+      ) : (
+        <div className="row">
+          <div className="mt-3 col-lg-12">
+            <div>
+              {" "}
+              <img
+                className="img-fluid w-50"
+                src="https://jersix.com/wp-content/uploads/2020/10/Empty-pana-uai-2000x1500.png"
+                alt=""
+              />
+              <h2 className="mt-3">Sorry, You have no order yet!</h2>
+              <div className="enroll_btn">
+                <button>
+                  <Link to="/courses">Please Go To some order</Link>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -7,84 +7,82 @@ import useAuth from "../hooks/useAuth";
 const Navigation = () => {
   const { user, logOut } = useAuth();
   return (
-    <nav
-      className="
-     navbar-expand-lg sticky-top navbar-light bg-light header"
-    >
-      <div className="container d-flex justify-content-between">
-        <Link className="navbar-brand" to="/">
-          <img src={logo} alt="" />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse me-auto"
-          id="navbarNavDropdown"
-        >
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link active" to="/courses">
-                Courses
-              </Link>
-            </li>{" "}
-            <li className="nav-item">
-              <Link className="nav-link active" to="/about_us">
-                About us
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link active" to="/blogs">
-                Blogs
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link active" to="/">
-                Pages
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link active" to="/myorders">
-                My Order
-              </Link>
-            </li>
-            <li className="nav-item">
-              {user?.email ? (
-                <div style={{ display: "flex" }}>
-                  <p className="nav-link active ">
-                    <i className="fas fa-user"></i> {user.displayName}
-                  </p>
-                  <p
-                    className="nav-link active"
-                    style={{ cursor: "pointer" }}
-                    onClick={logOut}
-                  >
-                    Logout <i className="fas fa-sign-out-alt"></i>
-                  </p>
-                </div>
-              ) : (
-                <Link className="nav-link active" to="/signup">
-                  <i className="fas fa-sign-out-alt"></i> SignUp
+    <>
+      {/* new nav */}
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <Link className="navbar-brand" to="/">
+            <img src={logo} alt="" />
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link active" to="/">
+                  Home
                 </Link>
-              )}
-            </li>
-          </ul>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/courses">
+                  Courses
+                </Link>
+              </li>{" "}
+              <li className="nav-item">
+                <Link className="nav-link active" to="/about_us">
+                  About us
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/blogs">
+                  Blogs
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/">
+                  Pages
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/myorders">
+                  My Order
+                </Link>
+              </li>
+              <li className="nav-item">
+                {user?.email ? (
+                  <div style={{ display: "flex" }}>
+                    <p className="nav-link active ">
+                      <i className="fas fa-user"></i> {user.displayName}
+                    </p>
+                    <p
+                      className="nav-link active"
+                      style={{ cursor: "pointer" }}
+                      onClick={logOut}
+                    >
+                      Logout <i className="fas fa-sign-out-alt"></i>
+                    </p>
+                  </div>
+                ) : (
+                  <Link className="nav-link active" to="/signup">
+                    <i className="fas fa-sign-out-alt"></i> SignUp
+                  </Link>
+                )}
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      {/* new nav */}
+    </>
   );
 };
 
